1c924e69833759040dff9de9dec985281eb74f69
Jamal Hasuev <metafra@mail.ru>
2021-04-25 18:43:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  App.vue
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: {
+  },
+  mutations: {
+  },
+  getters: {
+  },
+})
+export default store;
